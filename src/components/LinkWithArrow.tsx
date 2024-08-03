701070b662a43006b036749d26d2aa63d15d6e85
Cxx-mlr <spapiernik12@gmail.com>
2024-08-02 19:41:42 -0500

    feat(link): disable scroll behavior in LinkWithArrow component

--- src/components/LinkWithArrow.tsx
+++ src/components/LinkWithArrow.tsx
@@ -8,7 +8,9 @@ export default function LinkWithArrow({
   children: React.ReactNode
 }) {
   return (
-    <Link href="">
+    <Link href=""
+      scroll={false}
+    >
       <div className="group flex items-center gap-2">
         <p className="group-hover:text-yellow-700/40 tracking-tighter text-xl font-medium text-black/65">{children}</p>
         <FaArrowRight className="text-sm text-yellow-700/40 mt-[1.8px]" />
